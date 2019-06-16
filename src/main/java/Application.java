a26c7e6b1a1944b65e8cd9f50b49f9b2085307ec
yuriybon <yuriy_bon@mail.ru>
2019-06-16 12:43:40 +0200

    Test RSA

---
+++ src/main/java/Application.java
@@ -0,0 +1,41 @@
+import java.security.Key;
+
+public class Application {
+    public static long getAilerFunction() {
+        return 12;
+    }
+
+    static long power(long a, long b){
+        long result = 1;
+        for (int i=1; i<=b; i++){
+            result = result*a;
+        }
+        return result;
+    }
+
+    public static long encrypt(KeyPair publicKey, long message) {
+        long res = power(message,publicKey.key);
+        System.out.println(res);
+        return  res %publicKey.general;
+    }
+
+    public static long decrypt(KeyPair privateKey, long message) {
+        long res = power(message,privateKey.key);
+        
+        System.out.println(res);
+        return  res %privateKey.general;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        KeyPair publicKeyPair = new KeyPair(5,21);
+        KeyPair privateKeyPair = new KeyPair(17,21);
+        long message = 19;
+        long encryptedMessage = encrypt(publicKeyPair,message);
+        System.out.println(encryptedMessage);
+
+        long decryptedMessage = encrypt(privateKeyPair,encryptedMessage);
+        System.out.println(decryptedMessage);
+
+    }
+}
---
+++ src/main/java/KeyPair.java
@@ -0,0 +1,11 @@
+import java.security.Key;
+
+public class KeyPair {
+    Integer key;
+    Integer general;
+
+    public KeyPair(Integer key, Integer general) {
+        this.key = key;
+        this.general = general;
+    }
+}
